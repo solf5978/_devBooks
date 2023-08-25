1f0d55d990789cc49a3b9b363395a78cb538ea54
_temp <78365254+solf5978@users.noreply.github.com>
2023-08-25 11:49:08 +0800

    list in react

--- _theRoadToReact/hacker-stories/src/App.js
+++ _theRoadToReact/hacker-stories/src/App.js
@@ -27,9 +27,44 @@ import { getTTFB } from "web-vitals";
 
 // export default App;
 
+const list = [
+  {
+  title: `React`,
+  url: `https://reactjs.org/`,
+  author: `Jordan Walke`,
+  num_comments: 3,
+  points: 4,
+  objectID: 0,},
+  {
+    title: `Redux`,
+    url: `https://redux.js.org/`,
+    author: `Dan Abramov, Andrew Clark`,
+    num_comments: 2,
+    points: 5,
+    objectID: 1,},
+];
+
+// Vanilla Javascript
+
+const pureJS_List = () => {
+  for (let i = 0; i < list.length; i++) { return console.log(list[i].author)}}
+
+// React Version
+
+const reactJS_List = list.map( function(listItem) {
+  <h2>{listItem.title}</h2>
+});
+
+// Apply Object Key on React JS
+const aha = () => {
+  list.map ( function(listItem) {
+    return <h2> {listItem.author}</h2>
+  })
+}
+
 function getTitle(title) {
   return title;
-}
+}      
 
 const welcome = {
   greeting: `Hey`, 
@@ -41,9 +76,24 @@ const title = `React`;
 function App() {
   return (
     <div>
+      <h1> My Hacker Stories</h1>
       <h1>Hello function {getTitle("React")}</h1>
       <h1> Hello {title} World</h1>
       <h1> {welcome.greeting} {welcome.title} </h1>
+      <h1> VanillaJs </h1>
+      {pureJS_List}
+      <h1> React JS</h1>
+      {list.map( function(listItem) {
+        return <h2>{listItem.points}</h2>;
+      })}
+      console.log(aha)
+      <h1> Apply Object Key To React</h1>
+      {list.map(item => { return (
+        <div key={item.objectID}>
+          {item.title}
+        </div>
+      );
+      })}
       <label htmlFor="search"> Search: </label>
       <input- id="search" type="text" />
     </div>
