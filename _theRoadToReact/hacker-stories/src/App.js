adb02909a7be5ae41eb17938a427d9c8c375bd81
_temp <78365254+solf5978@users.noreply.github.com>
2023-08-28 11:11:28 +0800

    change to use react props

--- _theRoadToReact/hacker-stories/src/App.js
+++ _theRoadToReact/hacker-stories/src/App.js
@@ -24,94 +24,153 @@ import { getTTFB } from "web-vitals";
 //     </div>
 //   );
 // }
-
 // export default App;
-
-const list = [
-  {
-  title: `React`,
-  url: `https://reactjs.org/`,
-  author: `Jordan Walke`,
-  num_comments: 3,
-  points: 4,
-  objectID: 0,},
-  {
-    title: `Redux`,
-    url: `https://redux.js.org/`,
-    author: `Dan Abramov, Andrew Clark`,
-    num_comments: 2,
-    points: 5,
-    objectID: 1,},
-];
-
 // Vanilla Javascript
-
-const pureJS_List = () => {
-  for (let i = 0; i < list.length; i++) { return console.log(list[i].author)}}
-
+// const pureJS_List = () => {
+//   for (let i = 0; i < list.length; i++) { return console.log(list[i].author)}}
 // React Version
-
-const reactJS_List = list.map( function(listItem) {
-  <h2>{listItem.title}</h2>
-});
-
+// const reactJS_List = list.map( function(listItem) {
+//   <h2>{listItem.title}</h2>
+// });
 // Apply Object Key on React JS
-const aha = () => {
-  list.map ( function(listItem) {
-    return <h2> {listItem.author}</h2>
-  })
-}
+// const aha = () => {
+//   list.map ( function(listItem) {
+//     return <h2> {listItem.author}</h2>
+//   })
+// }
+// function getTitle(title) {
+//   return title;
+// }      
+// const welcome = {
+//   greeting: `Hey`, 
+//   title: `React`,
+// }
+// const title = `React`;
+// function App() {
+//   return (
+//     <div>
+//       <h1> My Hacker Stories</h1>
+//       <h1>Hello function {getTitle("React")}</h1>
+//       <h1> Hello {title} World</h1>
+//       <h1> {welcome.greeting} {welcome.title} </h1>
+//       <h1> VanillaJs </h1>
+//       {pureJS_List}
+//       <h1> React JS</h1>
+//       {list.map( function(listItem) {
+//         return null;
+//       })}
+//       <h1> Apply Object Key To React</h1>
+//       {list.map(item => { return (
+//         <div key={item.objectID}>
+//           {item.title}
+//         </div>);        } 
+//                 ) 
+//       }
 
-function getTitle(title) {
-  return null;
-}      
+//       <h1> More Than One Item </h1>
+//       {list.map( function(item) { 
+//         return (
+//         <div key={item.objectID}>
+//           <span>
+//             <a href={item.url}>{item.title}</a>
+//           </span>
+//           <span>{item.author}</span>
+//           <span>{item.num_comments}</span>
+//           <span>{item.points}</span>;
+//         </div> );
+//           })}
+        
 
-const welcome = {
-  greeting: `Hey`, 
-  title: `React`,
-}
+//       <label htmlFor="search"> Search: </label>
+//       <input id="search" type="text" />
 
-const title = `React`;
+//       <List />
+//     </div>
+//   );
+// }
+// function List() {
+//   return list.map(function(item) {
+//     return (
+//       <div key = {item.objectID}>
+//         <span>
+//           <a href={item.url}>{item.title}</a>
+//         </span>
+//         <span>{item.author}</span>
+//         <span>{item.num_comments}</span>
+//         <span>{item.points}</span>;
+//       </div>
+//     )
+//   })
+// }
 
-function App() {
+// const list = [
+//   {
+//   title: `React`,
+//   url: `https://reactjs.org/`,
+//   author: `Jordan Walke`,
+//   num_comments: 3,
+//   points: 4,
+//   objectID: 0,},
+//   {
+//     title: `Redux`,
+//     url: `https://redux.js.org/`,
+//     author: `Dan Abramov, Andrew Clark`,
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,},
+// ];
+
+// Applied Concise Version
+
+const List = props => 
+  props.list.map( item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>;
+    </div>
+  ))
+
+const App = () => {
+  const stories = [
+      {
+      title: `React`,
+      url: `https://reactjs.org/`,
+      author: `Jordan Walke`,
+      num_comments: 3,
+      points: 4,
+      objectID: 0,},
+      {
+        title: `Redux`,
+        url: `https://redux.js.org/`,
+        author: `Dan Abramov, Andrew Clark`,
+        num_comments: 2,
+        points: 5,
+        objectID: 1,},
+    ];
+    
+  const handleChange = event => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1> My Hacker Stories</h1>
-      <h1>Hello function {getTitle("React")}</h1>
-      <h1> Hello {title} World</h1>
-      <h1> {welcome.greeting} {welcome.title} </h1>
-      <h1> VanillaJs </h1>
-      {pureJS_List}
-      <h1> React JS</h1>
-      {list.map( function(listItem) {
-        return <h2>{listItem.points}</h2>;
-      })}
-      <h1> Apply Object Key To React</h1>
-      {list.map(item => { return (
-        <div key={item.objectID}>
-          {item.title}
-        </div>);        } 
-                ) 
-      }
 
-      <h1> More Than One Item </h1>
-      {list.map( function(item) { 
-        return (
-        <div key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>;
-        </div> );
-          })}
-        
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" onChange={handleChange} />
 
-      <label htmlFor="search"> Search: </label>
-      <input id="search" type="text" />
+      <hr />
+
+      <List list={stories} />
+
     </div>
-  );
+  )
 }
+
+
 
 export default App;
