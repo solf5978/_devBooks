ad32ae11f7b732e86892cc60bba66bf63f780714
_temp <78365254+solf5978@users.noreply.github.com>
2023-08-28 17:32:31 +0800

    introduced controlled component

--- _theRoadToReact/hacker-stories/src/App.js
+++ _theRoadToReact/hacker-stories/src/App.js
@@ -139,7 +139,7 @@ const Search = props => {
   return (
     <div>
       <label htmlFor="search">Search:</label>
-      <input id="search" type="text" onChange={props.onSearch} />
+      <input id="search" type="text" value={props.search} onChange={props.onSearch} />
     </div>
 
   )
@@ -175,7 +175,7 @@ const App = () => {
     <div>
       <h1> My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch} />
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
 
