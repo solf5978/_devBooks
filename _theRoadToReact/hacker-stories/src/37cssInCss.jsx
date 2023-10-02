27738fbf565218dd6f3cf15bc0080a3dab7f9a26
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-02 13:33:30 +0800

    adding button styles to module css

--- _theRoadToReact/hacker-stories/src/37cssInCss.jsx
+++ _theRoadToReact/hacker-stories/src/37cssInCss.jsx
@@ -3,6 +3,26 @@ import axios from 'axios';
 
 import styles from '../css/37cssInCss.css';
 
+const Item = ({ item, onRemoveItem }) => (
+    <li className={styles.item}>
+        <span style={{ width: '40%' }}>
+            <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '10%' }}>{item.num_comments}</span>
+        <span style={{ width: '10%' }}>{item.points}</span>
+        <span style={{ width: '10%' }}>
+        <button
+                type='button'
+                onClick={ () => onRemoveItem(item)}
+                className={`${styles.button} ${styles.buttonSmall}`}
+            >
+                Dismiss
+            </button>
+        </span>
+    </li>
+)
+
 const App = () => {
 
     return (
