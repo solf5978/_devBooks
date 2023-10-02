ba097c7e7c3277fb53cdc7ae6294f4e71adbdd2d
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-02 16:41:55 +0800

    applied nested css in styled component

--- _theRoadToReact/hacker-stories/src/38styledComponent.jsx
+++ _theRoadToReact/hacker-stories/src/38styledComponent.jsx
@@ -36,6 +36,24 @@ const StyledHeadlinePrimary = styled.h1`
     letter-spacing: 2px;
     `;
 
+const StyledItem = styled.li`
+    display: flex;
+    align-items: center;
+    padding-bottom: 5px;
+`
+const StyledColumn = styled.span`
+    padding: 0 5px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    a {
+        color: inherit;
+    }
+
+    width: ${(props) => props.width};
+`
+
 const App = () => {
     return (
         <StyledContainer>
