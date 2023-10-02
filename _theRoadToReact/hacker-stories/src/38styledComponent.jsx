4a62057a1b3aac425c8b239d290cc0442e803495
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-02 16:36:23 +0800

    applied styled componentn inside the react component

--- _theRoadToReact/hacker-stories/src/38styledComponent.jsx
+++ _theRoadToReact/hacker-stories/src/38styledComponent.jsx
@@ -2,6 +2,24 @@ import * as React from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 
+const Item = ({ item, onRemoveItem }) => (
+    <StyledItem>
+        <StyledColumn width="40%">
+            <a href={item.url}>{item.title}</a>
+        </StyledColumn>
+        <StyledColumn width="30%">{item.author}</StyledColumn>
+        <StyledColumn width="10%">{item.num_comments}</StyledColumn>
+        <StyledColumn width="10%">{item.points}</StyledColumn>
+        <StyledColumn width="10%">
+            <StyledButtonSmall
+                type="button"
+                onClick={() => onRemoveItem(item)}
+            > Dismiss
+            </StyledButtonSmall>
+        </StyledColumn>
+    </StyledItem>
+    )
+
 const StyledContainer = styled.div`
     height: 100vw;
     padding: 20px;
@@ -17,3 +35,23 @@ const StyledHeadlinePrimary = styled.h1`
     font-weight: 300;
     letter-spacing: 2px;
     `;
+
+const App = () => {
+    return (
+        <StyledContainer>
+            <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
+            <SearchForm
+                searchTerm={searchTerm}
+                onSearchInput={handleSearchInput}
+                onSearchSubmit={handleSearchSubmit}
+            />
+
+            {stories.isError && <p>Something went wrong...</p>}
+            {stories.isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <List list={stories.data} onRemoveItem={handleRemoveStory}/>
+            )}
+        </StyledContainer>
+    )
+}
