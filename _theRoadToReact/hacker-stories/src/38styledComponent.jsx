96d51db11e61ab87eba2f9cfb8f3007436df34cf
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-02 15:41:11 +0800

    applied styled compoennt on 38, and installed on VSCode

--- _theRoadToReact/hacker-stories/src/38styledComponent.jsx
+++ _theRoadToReact/hacker-stories/src/38styledComponent.jsx
@@ -2,4 +2,18 @@ import * as React from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 
+const StyledContainer = styled.div`
+    height: 100vw;
+    padding: 20px;
+    
+    background: #83A4D4;
+    background: linear-gradient(to left, #b6fbff, #83A4D4);
+    
+    color: #171212;
+    `;
 
+const StyledHeadlinePrimary = styled.h1`
+    font-size: 2rem;
+    font-weight: 300;
+    letter-spacing: 2px;
+    `;
