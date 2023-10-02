052745f1169621ba41bbc82f2a5f2be74be45f34
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-02 13:23:19 +0800

    adding bakc the item component using cssInCss technique to deco the classnName of button

--- _theRoadToReact/hacker-stories/src/37cssInCss.jsx
+++ _theRoadToReact/hacker-stories/src/37cssInCss.jsx
@@ -3,3 +3,24 @@ import axios from 'axios';
 
 import styles from '../css/37cssInCss.css';
 
+const App = () => {
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.headlinePrimary}>My Hacker Stories</h1>
+
+            <SearchForm
+                searchTerm={searchTerm}
+                onSearchInput={handleSearchInput}
+                onSearchSubmit={handleSearchSubmit}
+            />
+
+            {stories.isError && <p>Something went wrong...</p>}
+            {stories.isLoading ? (
+                <p>Loading...</p>
+            ) : (
+                <List list={stories.data} onRemoveItem={handleRemoveStory}/>
+            )}
+        </div>
+    )
+}
