d4c6b43189003b662b3f8b2c30eb2060e27586e7
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-02 13:18:47 +0800

    adding back the main part of app, and calling the css style thru imported css file using regular html element div h1 ...etc

--- _theRoadToReact/hacker-stories/src/36cssInReact.jsx
+++ _theRoadToReact/hacker-stories/src/36cssInReact.jsx
@@ -2,6 +2,63 @@ import * as React from 'react'
 
 import '../css/36cssInReact.css'
 
+// Inline Style Approach
+const Item = ({ item, onRemoveItem }) => (
+    <li className="item">
+        <span style={{ width: '40%' }}>
+            <a href={item.url}>{item.title}</a>
+        </span>
+
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '10%' }}>{item.num_comments}</span>
+        <span style={{ width: '10%' }}>{item.points}</span>
+        <span style={{ width: '10%' }}>
+            <button
+                type='button'
+                onClick={ () => onRemoveItem(item)}
+                className='button button_small'
+            >
+                Dismiss
+            </button>
+        </span>
+    </li>
+)
+
+const InputWithLabel = ({ }) => {
+
+    return (
+        <>
+            <label htmlFor={id} className='label'>
+                {children}
+            </label>
+            &nbsp;
+            <input
+            ref={inputRef}
+            id={id}
+            type={type}
+            value={value}
+            onChange={onInputChange}
+            className='input'
+            />
+        </>
+    )
+}
+
+const SearchForm = ({}) => (
+    <form onSubmit={onSearchSubmit} className='search-form'>
+        <InputWithLabel>
+            <strong>Search: </strong>
+        </InputWithLabel>
+
+        <button
+            type="submit"
+            disabled={!searchTerm}
+            className='button button_large'>
+            Submit
+        </button>
+    </form>
+)
+
 const App = () => {
 
     const handleSearchInput = () => {
---
+++ _theRoadToReact/hacker-stories/src/37cssInCss.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import axios from 'axios';
+
+import styles from '../css/37cssInCss.css';
+
