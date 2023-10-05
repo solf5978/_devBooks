bf9bfcccf01cde0b3e63abf8c10bf00fe62c2245
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-05 10:36:31 +0800

    toggle flag to prevent useEffect running twice

--- _theRoadToReact/hacker-stories/src/40perfInReact.jsx
+++ _theRoadToReact/hacker-stories/src/40perfInReact.jsx
@@ -7,6 +7,22 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </React.StrictMode>
 )
 
+const useStorageState = (key, initialState) => {
+    const isMounted = React.useRef(false)
+    const [value, setValue] = React.useState(
+        localStorage.getItem(key) || initialState
+    )
+
+    React.useEffect(() => {
+        if (!isMounted.current) {
+            isMounted.current = true
+        } else {
+            console.log('A')
+            localStorage.setItem(key, value)
+        }
+    }, [value, key])
+}
+
 const App = () => {
     React.useEffect(() => {
         console.log('How Many TImes Do I Log')
