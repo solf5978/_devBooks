5f9c8cd9cad002fa471ead220dce3aaa4aef44ec
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-05 17:09:55 +0800

    replace reactDOM.render to createRoot().renderand get deep inside what return from a component does

--- _pureReact/react-hello/src/index.js
+++ _pureReact/react-hello/src/index.js
@@ -1,5 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
+
+// What JSX Turns Into
+// By Babel's Compiler →　JavaScript
+
+// React.createElement(
+//   string|Element,    ←　HTML or SVG Tags or Components
+//   [propsObject],     ←　
+//   [children...]
+// )
+
+// Nested
+// React.createElement('div', {},
+//  React.createElement('div', {}, 'Child1'),
+//    React.createElement('div', {}, 'Child2',
+//      React.createElement('div', {}, 'Child2_child')))
 
 function HelloWorld() {
   return (
@@ -7,7 +22,20 @@ function HelloWorld() {
   )
 }
 
-ReactDOM.render(
-  <HelloWorld/>,
-  document.querySelector('#root')
-)
+function ReHelloWorld() {
+  return (
+    React.createElement(
+      'div',
+      {},
+      'Hello World'
+    )
+  )
+}
+
+// Syntax
+// ReactDOM.render([React Element], [DOM Element])
+
+const domNode = document.getElementById('root')
+const root = createRoot(domNode)
+
+root.render(<ReHelloWorld />)
