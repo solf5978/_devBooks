ae3bcf533e2c9f357041995325dc4bdd18918c26
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-05 13:58:21 +0800

    go thru diff. approaches of using react in TypeScript

--- _theRoadToReact/hacker-stories/src/41typeScriptInReact.tsx
+++ _theRoadToReact/hacker-stories/src/41typeScriptInReact.tsx
@@ -9,3 +9,88 @@ ReactDOM.createRoot(
         <App />
     </React.StrictMode>
 )
+
+const useStorageState = (
+    key: string,
+    initialState: string
+): [string, (newValue: string) => void] => {
+    const [value, setValue] = React.useState(
+        localStorage.getItem(key) || initialState
+    )
+
+    React.useEffect(() => {
+        localStorage.setItem(key, value)
+    }, [value, key])
+    return [value, setValue]
+}
+
+// const Item = ({
+//     item,
+//     onRemoveItem,
+// }: {
+//     item: {
+//         objectID: string
+//         url: string
+//         title: string
+//         author: string
+//         num_comments: number
+//         points: number
+//     }
+//     onRemoveItem: (item: {
+//         objectID: string
+//         url: string
+//         title: string
+//         author: string
+//         num_comments: number
+//         points: number
+//     }) => void
+// }) => (
+//     <li>
+        
+//     </li>
+// )
+
+type Story = {
+    objectID: string
+    url: string
+    title: string
+    author: string
+    num_comments: number
+    points: number
+}
+
+
+// const Item = ({
+//     item,
+//     onRemoveItem,
+// }: {
+//     item: Story
+//     onRemoveItem: (item: Story) => void
+// }) => (
+//     <li>
+
+//     </li>
+// )
+// })
+
+type ItemProps = {
+    item: Story
+    onRemoveItem: (item: Story) => void
+}
+
+// const Item = ({
+//     item, 
+//     onRemoveItem }: ItemProps) => (
+//         <li>
+
+//         </li>
+//     )
+
+const Item: React.FC<ItemProps> = ({ 
+    item, 
+    onRemoveItem 
+}): JSX.Element => (
+    <li>
+
+    </li>
+)
