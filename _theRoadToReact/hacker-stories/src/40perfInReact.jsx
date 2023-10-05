711b03d51c1fd2fb34ce059354d34c8fb98bcca1
_temp <78365254+solf5978@users.noreply.github.com>
2023-10-05 11:42:37 +0800

    reduced unnecessary invocation of function to reduce the perf costs

--- _theRoadToReact/hacker-stories/src/40perfInReact.jsx
+++ _theRoadToReact/hacker-stories/src/40perfInReact.jsx
@@ -7,6 +7,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </React.StrictMode>
 )
 
+const getSumComments = (stories) => {
+    console.log('C')
+
+    return stories.data.reduce(
+        (result, value) => result + value.num_comments,
+        0
+    )
+}
+
 const useStorageState = (key, initialState) => {
     const isMounted = React.useRef(false)
     const [value, setValue] = React.useState(
@@ -23,9 +32,40 @@ const useStorageState = (key, initialState) => {
     }, [value, key])
 }
 
+const List = React.memo(
+    ({ list, onRemoveItem }) => 
+    console.log('B:List') || (
+    <ul>
+        {list.map((item) => (
+            <item
+                key={item.objectID}
+                item={item}
+                onRemoveItem={onRemoveItem}
+            />
+        ))}
+    </ul>
+))
+
 const App = () => {
+    console.log('B:App')
+
+    const sumComments = getSumComments(stories)
+
     React.useEffect(() => {
         console.log('How Many TImes Do I Log')
         handleFetchStories();
     }, [handleFetchStories])
+    
+    const handleRemoveStory = React.useCallback((item) => {
+        dispatchStories({
+            type: 'REMOVE_STORY',
+            payload: item,
+        })
+    }, [])
+
+    return (
+        <div>
+            <h1>My Hacker Stories with { sumComments } comments.</h1>
+        </div>
+    )
 }
