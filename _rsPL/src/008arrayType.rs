43507133bb5fadde51bd437610fdcf66793f07f9
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-13 14:45:43 +0800

    array, same type and fixed size

--- _rsPL/src/008arrayType.rs
+++ _rsPL/src/008arrayType.rs
@@ -18,4 +18,10 @@ fn main() {
     let a: [i32; 5] = [1, 2, 3, 4, 5];
     let a = [3; 5];
     let a = [3, 3, 3, 3, 3]; // same as above
+
+    // accessing array elements
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
 }
