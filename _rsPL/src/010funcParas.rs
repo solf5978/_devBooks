0a7091fe9945cf13a4efcb70f82216669e4475c8
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-13 15:03:54 +0800

    parameters divided by comma

--- _rsPL/src/010funcParas.rs
+++ _rsPL/src/010funcParas.rs
@@ -5,3 +5,6 @@ fn main() {
 fn another_function(x: i32) {
     println!("The value of x is: {x}");
 }
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
