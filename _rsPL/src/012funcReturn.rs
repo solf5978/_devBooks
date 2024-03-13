685cbee190b51331c8d0ed6a425ba5b4462fe3fc
_temp <78365254+solf5978@users.noreply.github.com>
2024-03-13 15:32:08 +0800

    play with return / expression

--- _rsPL/src/012funcReturn.rs
+++ _rsPL/src/012funcReturn.rs
@@ -6,4 +6,11 @@ fn main() {
     let x = five();
 
     println!("The value of x is: {x}");
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
