5953b77759c760b7f726482c6e2718c48363fdab
_temp <78365254+solf5978@users.noreply.github.com>
2024-04-02 22:29:35 +0800

    starting with a gcd program

--- _rsPL/src/main.rs
+++ _rsPL/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // let b = Box::new(0);
+    // let b2 = b;
+    // move_a_box(b);
+
+    // let b = Box::new(0);
+    // move_a_box(b);
+    // let b2 = b;
+
+    // None of these snippets
+
+    // let b = Box::new(0);
+    // move_a_box(b);
+    // println!("{}", b);
+
+    let b = Box::new(0);
+    let b2 = b;
+    println!("{}", b);
+    move_a_box(b2);
+}
+
+fn move_a_box(b: Box<i32>) {
+    // This space intentionally left blank
 }
