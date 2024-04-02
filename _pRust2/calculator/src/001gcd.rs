24292396a4ea6ddc366fe2eab8cc3adf57e19d5e
_temp <78365254+solf5978@users.noreply.github.com>
2024-04-02 23:00:10 +0800

    move function to standalone file

---
+++ _pRust2/calculator/src/001gcd.rs
@@ -0,0 +1,24 @@
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+#[test]
+fn test_gcd() {
+    assert_eq!(gcd(12, 18), 6);
+    assert_eq!(gcd(18, 12), 6);
+    assert_eq!(gcd(12, 24), 12);
+    assert_eq!(gcd(24, 12), 12);
+    assert_eq!(gcd(12, 0), 12);
+    assert_eq!(gcd(0, 12), 12);
+    assert_eq!(gcd(0, 0), 0);
+    assert_eq!(gcd(12, 12), 12);
+}
--- _pRust2/calculator/src/main.rs
+++ _pRust2/calculator/src/main.rs
@@ -1,28 +1,19 @@
+use std::env;
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
-}
-
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if m < n {
-            let t = m;
-            m = n % m;
-            n = t;
-        }
-        m = m % n;
+    let mut numbers = Vec::new();
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing arguments"));
     }
-    n
-}
 
-#[test]
-fn test_gcd() {
-    assert_eq!(gcd(12, 18), 6);
-    assert_eq!(gcd(18, 12), 6);
-    assert_eq!(gcd(12, 24), 12);
-    assert_eq!(gcd(24, 12), 12);
-    assert_eq!(gcd(12, 0), 12);
-    assert_eq!(gcd(0, 12), 12);
-    assert_eq!(gcd(0, 0), 0);
-    assert_eq!(gcd(12, 12), 12);
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER1 NUMBER2");
+        std::process::exit(1);
+    }
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+    println!("The Greatest Common Divisor of {:?} is {}", numbers, d);
 }
