f85298b29f7b29244f91d737aca11bffed08d798
_temp <78365254+solf5978@users.noreply.github.com>
2024-04-04 15:54:40 +0800

    switch to actix 4.1

--- _pRust2/actix-gcd/src/main.rs
+++ _pRust2/actix-gcd/src/main.rs
@@ -1,47 +1,65 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
-use serde::Deserialize;
 
-#[derive(Deserialize)]
-struct GcdParameter {
-    n: u64,
-    m: u64,
-}
+#[actix_web::main]
+async fn main() {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
+    });
 
-fn main() {
-    let server = HttpServer::new(|| App::new().route("/", web::get().to(get_index)));
     println!("Serving on http://localhost:3000...");
     server
         .bind("127.0.0.1:3000")
         .expect("error binding server to address")
         .run()
+        .await
         .expect("error running server");
 }
 
-fn get_index() -> HttpResponse {
+async fn get_index() -> HttpResponse {
     HttpResponse::Ok().content_type("text/html").body(
         r#"
-            <title>GCD Calculator</title>
-            <form action="/gcd" method="post">
-            <input type="text" name="n" placeholder="Enter a number">
-            <input type="text" name="m" placeholder="Enter another number">
-            <button type="submit">Compute GCD</button>
-            </form>
+                <title>GCD Calculator</title>
+                <form action="/gcd" method="post">
+                <input type="text" name="n"/>
+                <input type="text" name="m"/>
+                <button type="submit">Compute GCD</button>
+                </form>
             "#,
     )
 }
 
-fn post_gcd(form: web::Form<GcdParameter>) -> HttpResponse {
+use serde::Deserialize;
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
     if form.n == 0 || form.m == 0 {
         return HttpResponse::BadRequest()
             .content_type("text/html")
-            .body("Computing the GCD with zero is not allowed.");
+            .body("Computing the GCD with zero is boring.");
     }
 
-    let rep = format!(
-        "The Greatest Common Divisor of the numbers {} and {} is <b>{}</b>\n",
+    let response = format!(
+        "The greatest common divisor of the numbers {} and {} \
+                 is <b>{}</b>\n",
         form.n,
         form.m,
         gcd(form.n, form.m)
     );
-    HttpResponse::Ok().content_type("text/html").body(rep)
+
+    HttpResponse::Ok().content_type("text/html").body(response)
+}
+
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        std::mem::swap(&mut m, &mut n);
+        m %= n;
+    }
+    n
 }
