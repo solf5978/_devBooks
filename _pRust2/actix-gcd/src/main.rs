efd0380c95c87f64070deef986c82d73d5a96f3d
_temp <78365254+solf5978@users.noreply.github.com>
2024-04-04 14:59:50 +0800

    the crate from older version doesn't create the trait it needs

--- _pRust2/actix-gcd/src/main.rs
+++ _pRust2/actix-gcd/src/main.rs
@@ -1,20 +1,24 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameter {
+    n: u64,
+    m: u64,
+}
 
 fn main() {
-    let server = HttpServer::new(|| {
-        App::new()
-           .route("/", web::get().to(get_index))
-    });
+    let server = HttpServer::new(|| App::new().route("/", web::get().to(get_index)));
     println!("Serving on http://localhost:3000...");
     server
-    .bind("127.0.0.1:3000").expect("error binding server to address");
-    .run().expect("error running server");
+        .bind("127.0.0.1:3000")
+        .expect("error binding server to address")
+        .run()
+        .expect("error running server");
 }
 
 fn get_index() -> HttpResponse {
-    HttpResponse::Ok()
-       .content_type("text/html")
-       .body(
+    HttpResponse::Ok().content_type("text/html").body(
         r#"
             <title>GCD Calculator</title>
             <form action="/gcd" method="post">
@@ -23,5 +27,21 @@ fn get_index() -> HttpResponse {
             <button type="submit">Compute GCD</button>
             </form>
             "#,
-       )
+    )
+}
+
+fn post_gcd(form: web::Form<GcdParameter>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is not allowed.");
+    }
+
+    let rep = format!(
+        "The Greatest Common Divisor of the numbers {} and {} is <b>{}</b>\n",
+        form.n,
+        form.m,
+        gcd(form.n, form.m)
+    );
+    HttpResponse::Ok().content_type("text/html").body(rep)
 }
