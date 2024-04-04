7fa00c12f4f6bd915b1fc178f6e908de2729663b
_temp <78365254+solf5978@users.noreply.github.com>
2024-04-04 18:21:11 +0800

    introduced escape timing

--- _pRust2/mandelbrot/src/main.rs
+++ _pRust2/mandelbrot/src/main.rs
@@ -1,4 +1,9 @@
 use num::Complex;
+// struct defined inside num::Complex
+// struct Complex<T> {
+//     re: T,
+//     im: T,
+// }
 
 fn complex_square_add_loop(c: Complex<f64>) {
     let mut z = Complex { re: 0.0, im: 0.0 };
@@ -8,4 +13,21 @@ fn complex_square_add_loop(c: Complex<f64>) {
             break;
         }
     }
+}
+
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    let mut counter = 0;
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+        z = z * z + c;
+    }
+    None
+}
+
+fn main() {
+    let c = Complex { re: 3.0, im: 4.0 };
+    complex_square_add_loop(c);
 }
