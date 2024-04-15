ea1415d04f6952f3903a5cd4185a7dde5ebe94d4
hsiao.maxim <78365254+solf5978@users.noreply.github.com>
2024-04-15 21:35:57 +0800

    add [[bin]] name&path to cargo, planted some code in tcpserver to have a basic function working on then cargo run -p (package) tcpserver

--- _rServicesApps/tcpserver/src/main.rs
+++ _rServicesApps/tcpserver/src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let connection_listener = TcpListener::bind("127.0.0.1:3456").unwrap();
+    println!("Running on port: 3456");
+
+    for stream in connection_listener.incoming() {
+        let _stream = stream.unwrap();
+        println!("Connection Established");
+    }
 }
