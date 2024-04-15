6231f5122b70d39a00e8c152327caae18f4e7dde
hsiao.maxim <78365254+solf5978@users.noreply.github.com>
2024-04-15 22:03:32 +0800

    finished client reading msg from server side

--- _rServicesApps/tcpclient/src/main.rs
+++ _rServicesApps/tcpclient/src/main.rs
@@ -1,5 +1,14 @@
+use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::str;
 
 fn main() {
-    let _stream = TcpStream::connect("localhost:3456").unwrap();
+    let mut stream = TcpStream::connect("localhost:3456").unwrap();
+    stream.write("Hello, Rust Web".as_bytes()).unwrap();
+    let mut buffer = [0; 16];
+    stream.read(&mut buffer).unwrap();
+    println!(
+        "Got Repsonse From Server: {:?}",
+        str::from_utf8(&buffer).unwrap()
+    );
 }
