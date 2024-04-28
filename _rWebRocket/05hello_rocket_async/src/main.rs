83936940955417b6c5b21fac6334b0ad1f2af6ac
hsiao.maxim <78365254+solf5978@users.noreply.github.com>
2024-04-28 13:24:58 +0800

    intro to Rocket toml

---
+++ _rWebRocket/05hello_rocket_async/src/main.rs
@@ -0,0 +1,14 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+// alternative way
+// #[route(GET, path = "/")]
+async fn index() -> &'static str {
+    "Hello, Rocket!!"
+}
+
+#[rocket::main]
+async fn main() {
+    rocket::build().mount("/", routes![index]).launch().await;
+}
