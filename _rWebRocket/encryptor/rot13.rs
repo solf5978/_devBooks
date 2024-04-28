7d7c1a8ebe5855dc8280b0cae0eadb1cc98b4b59
hsiao.maxim <78365254+solf5978@users.noreply.github.com>
2024-04-28 10:44:20 +0800

    refine using map

--- _rWebRocket/encryptor/rot13.rs
+++ _rWebRocket/encryptor/rot13.rs
@@ -1,7 +1,35 @@
 pub struct Rot13(pub String);
 impl super::Encryptable for Rot13 {
-    fn encrypt(&self) -> String {
+    fn encrypt(&self) -> Result<String, Box<dyn Error>> {
         let mut new_string = String::new();
+        // Third Intro
+        self.0
+        .chars().map( |ch| {
+            if (ch >= 'a' && ch < 'n') || (ch >= 'A' && ch < 'N') {
+                (ch as u8 + 13) as char
+            } else if (ch >= 'n' && ch < 'z') || (ch >= 'N' && ch < 'Z') {
+                (ch as u8 - 13) as char
+            } else {
+                ch
+            }
+        }).collect()
+    }
+
+        // Second Intro
+        // let mut new_string = String::new();
+        // for ch in self.0.chars() {
+        //     if (ch >= 'a' && ch < 'n') || (ch >= 'A' && ch < 'N') {
+        //         new_string.push((ch as u8 + 13) as char);
+        //     } else if (ch >= 'n' && ch < 'z') || (ch >= 'N' && ch < 'Z') {
+        //         new_string.push((ch as u8 - 13) as char);
+        //     } else {
+        //         new_string.push(ch);
+        //     }
+        // }
+        // new_string
+
+        // First Intro
+        // let mut new_string = String::new();
         // let len = self.0.len();
         // for i in 0..len {
         //     if (self.0[i] >= 'a' && self.0[i] < 'n') || (self.0[i] >= 'A' && self.0[i] < 'N') {
@@ -14,15 +42,5 @@ impl super::Encryptable for Rot13 {
         //     }
         // }
         // new_string
-        for ch in self.0.chars() {
-            if (ch >= 'a' && ch < 'n') || (ch >= 'A' && ch < 'N') {
-                new_string.push((ch as u8 + 13) as char);
-            } else if (ch >= 'n' && ch < 'z') || (ch >= 'N' && ch < 'Z') {
-                new_string.push((ch as u8 - 13) as char);
-            } else {
-                new_string.push(ch);
-            }
-        }
-        new_string
     }
 }
