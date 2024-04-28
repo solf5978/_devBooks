7397c3922a622eee281791b47aa098cfc9602f0a
hsiao.maxim <78365254+solf5978@users.noreply.github.com>
2024-04-28 11:13:28 +0800

    refine project structure

--- _rWebRocket/02_complex_program/main.rs
+++ _rWebRocket/02_complex_program/main.rs
@@ -1,9 +1,16 @@
+pub mod encryptor;
+use encryptor::Encryptable;
+// use crate::encryptor::Encryptable;
 use std::io;
+
 fn main() {
     println!("Input the string you want to encrypt");
     let mut user_input = String::new();
     io::stdin()
         .read_line(&mut user_input)
         .expect("Cannot read input");
-    println!("Your encrypted string: {}", user_input);
+    println!(
+        "Your encrypted string: {}",
+        encryptor::rot13::Rot13(user_input).encrypt()
+    );
 }
