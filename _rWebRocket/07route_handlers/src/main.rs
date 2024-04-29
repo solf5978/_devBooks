699e029ca6005c31dfa4a1506c11a2ab4f402a41
hsiao.maxim <78365254+solf5978@users.noreply.github.com>
2024-04-29 11:43:19 +0800

    add up lazy static, which calculate the stateic at runtime, and allow hashmap to be assigned to a static variable

--- _rWebRocket/07route_handlers/src/main.rs
+++ _rWebRocket/07route_handlers/src/main.rs
@@ -1,7 +1,8 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::{Build, Rocket};
+use rocket::{Build, Rocket};z
+use std::collections::HashMap;
 
 struct Filters {
     age: u8,
@@ -9,10 +10,24 @@ struct Filters {
 }
 
 #[derive(Debug)]
-struct User {
+struct User{
     uuid: String,
     name: String,
     age: u8,
     grade: u8,
     active: bool,
+}
+
+static USERS: HashMap<&str, User> = {
+    let map = HashMap::new();
+    map.insert(
+        "UUID_1",
+        User {
+            uuid: String::from("UUID_1"),
+            name: String::from("Hello Rocket"),
+            age: 10,
+            grade: 1,
+            active: true,
+        },
+    );
 }
