d8e3a63286873b236e7e9ca68320f3405fecec1e
hsiao.maxim <78365254+solf5978@users.noreply.github.com>
2024-04-29 14:59:12 +0800

    p70

--- _rWebRocket/07route_handlers/src/main.rs
+++ _rWebRocket/07route_handlers/src/main.rs
@@ -2,12 +2,40 @@
 extern crate rocket;
 
 use lazy_static::lazy_static;
+use rocket::request::FromParam;
 use rocket::{Build, Rocket};
 use std::collections::HashMap;
+use std::vec::Vec;
 
+#[derive(FromForm)]
 struct Filters {
     age: u8,
     active: bool,
+}
+
+struct NameGrade<'r> {
+    name: &'r str,
+    grade: u8,
+}
+
+impl<'r> FromParam<'r> for NameGrade<'r> {
+    type Error = &'static str;
+
+    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
+        const ERROR_MESSAGE: Result<NameGrade, &'static str> = Err("Error parsing user parameter");
+
+        let name_grade_vec: Vec<&'r str> = param.split('_').collect();
+        match name_grade_vec.len() {
+            2 => match name_grade_vec[1].parse::<u8>() {
+                Ok(n) => Ok(Self {
+                    name: name_grade_vec[0],
+                    grade: n,
+                }),
+                Err(_) => ERROR_MESSAGE,
+            },
+            _ => ERROR_MESSAGE,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -23,10 +51,10 @@ lazy_static! {
     static ref USERS: HashMap<&'static str, User> = {
         let mut map = HashMap::new();
         map.insert(
-            "UUID",
+            "3e3dd4ae-3c37-40c6-aa64-7061f284ce28",
             User {
-                uuid: "UUID".to_string(),
-                name: "Name".to_string(),
+                uuid: String::from("3e3dd4ae-3c37-40c6-aa64-7061f284ce28"),
+                name: String::from("John Doe"),
                 age: 18,
                 grade: 1,
                 active: true,
@@ -40,7 +68,36 @@ lazy_static! {
 fn user(uuid: &str) -> String {
     let user = USERS.get(uuid);
     match user {
-        Some(u) => format!("Found user {:?}", u),
+        Some(u) => format!("Found user: {:?}", u),
         None => String::from("User not found"),
     }
+}
+
+#[get("/users/<name_grade>?<filters..>")]
+fn users(name_grade: NameGrade, filters: Option<Filters>) -> String {
+    let users: Vec<&User> = USERS
+        .values()
+        .filter(|user| user.name.contains(&name_grade.name) && user.grade == name_grade.grade)
+        .filter(|user| {
+            if let Some(fts) = &filters {
+                user.age == fts.age && user.active == fts.active
+            } else {
+                true
+            }
+        })
+        .collect();
+    if users.is_empty() {
+        String::from("No user found")
+    } else {
+        users
+            .iter()
+            .map(|u| format!("{:?}", u))
+            .collect::<Vec<String>>()
+            .join(",")
+    }
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build().mount("/", routes![user, users])
 }
